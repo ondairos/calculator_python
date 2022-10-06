457da6fbd0b011b61db1cfef7713acd2baa0b402
Ondairos <ondairos@gmail.com>
2022-10-06 13:39:51 +0300

    recursive function to start the program again

--- calculator.py
+++ calculator.py
@@ -24,24 +24,27 @@ operations = {
     "/": divide,
 }
 
-num1 = int(input("What's the first number?: "))
+def calculator(): #recursive function to start the program again
+    num1 = int(input("What's the first number?: "))
 
-#display the symbols
-for symbols in operations:
-    print(symbols)
+    #display the symbols
+    for symbols in operations:
+        print(symbols)
 
-should_continue = True #flag
+    should_continue = True #flag
 
-while should_continue:
-    operation_symbol = input("Pick an operation: ")
-    num2 = int(input("What's the next number?: "))
-    calc_function = operations[operation_symbol] #use the function stored inside the dictionary with 1 line of code
-    answer = calc_function(num1, num2) #store the answer
+    while should_continue:
+        operation_symbol = input("Pick an operation: ")
+        num2 = int(input("What's the next number?: "))
+        calc_function = operations[operation_symbol] #use the function stored inside the dictionary with 1 line of code
+        answer = calc_function(num1, num2) #store the answer
 
-    print(f"{num1} {operation_symbol} {num2} = {answer}")
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
 
-    if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to exit: ") == "y":
-        num1 = answer
-    else:
-        should_continue = False
+        if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to continue with a new calculation: ") == "y":
+            num1 = answer
+        else:
+            should_continue = False
+            calculator()
     
+calculator()
