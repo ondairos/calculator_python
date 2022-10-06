1937a1bf164ed1359ba44897b719deaf90d0fa90
Ondairos <ondairos@gmail.com>
2022-10-06 13:18:23 +0300

    refactored the operation function into using the functions inside the dictionary

--- calculator.py
+++ calculator.py
@@ -32,17 +32,18 @@ for symbols in operations:
 
 operation_symbol = input("Pick an operation from the line above: ")
 
-def operate (num1, num2, operation_symbol):
-    if operation_symbol == "+":
-        final_value = add(num1,num2)
-    elif operation_symbol == "-":
-        final_value = subtract(num1,num2)
-    elif operation_symbol == "*":
-        final_value= multiply(num1,num2)
-    elif operation_symbol == "/":
-        final_value = divide(num1,num2)
-    return final_value 
+# def operate (num1, num2, operation_symbol):
+#     if operation_symbol == "+":
+#         final_value = add(num1,num2)
+#     elif operation_symbol == "-":
+#         final_value = subtract(num1,num2)
+#     elif operation_symbol == "*":
+#         final_value= multiply(num1,num2)
+#     elif operation_symbol == "/":
+#         final_value = divide(num1,num2)
+#     return final_value 
 
+calc_function = operations[operation_symbol] #use the function stored inside the dictionary with 1 line of code
+answer = calc_function(num1, num2) #store the answer
 
-answer = operate(num1,num2,operation_symbol)
 print(f"{num1} {operation_symbol} {num2} = {answer}")
